eecd2b7dc9570eb4c6ce308de10d2c53c8d20376
Alimary28 <alexandru28.pa@gmail.com>
2021-01-25 20:35:15 +0200

    Initial Commit

---
+++ Infrastructure/Data/FoodContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class FoodContext : DbContext
+    {
+        public FoodContext(DbContextOptions<FoodContext> options): base(options)
+        { 
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
