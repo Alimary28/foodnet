8f0b2cf8bfebaba51c4b4a03785eea62836ab52a
Alimary28 <alexandru28.pa@gmail.com>
2021-02-19 20:15:39 +0200

    Added pagination, sorting and filtering for products

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,12 @@ namespace API
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
 
+            services.AddCors(opt =>
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                }));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,11 +58,14 @@ namespace API
             //}
 
             app.UseMiddleware<ExceptionMiddleware>();
+
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -2,16 +2,46 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams) : base(x =>
+            //(string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+            (string.IsNullOrEmpty(productParams.Search) || x.Description.ToLower().Contains(productParams.Search)) &&
+            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
+            AddOrderBy(x => x.Name);
+            ApplyPagination(productParams.PageSize * (productParams.PageIndex - 1), productParams.PageSize);
+
+            if (!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    case "productType":
+                        AddOrderBy(t => t.ProductType.Name);
+                        break;
+                    case "productBrand":
+                        AddOrderBy(b => b.ProductBrand.Name);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
