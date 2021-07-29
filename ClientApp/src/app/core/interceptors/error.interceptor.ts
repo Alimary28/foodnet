5e797134faf5471609e009b38315ab125b71af86
Alimary28 <alexandru28.pa@gmail.com>
2021-07-29 12:11:53 +0300

    Handling the errors received from API with the help of an Interceptor component

---
+++ ClientApp/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,46 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router, NavigationExtras } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router, private toastr: ToastrService) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error) {
+          if (error.status === 400) {
+            error.statusText = 'Bad request';
+            if (error.error.errors) {
+              throw error.error;
+            } else {
+              this.toastr.error(error.error.message, error.error.statusCode);
+            }
+          }
+          if (error.status === 401) {
+            error.statusText = 'Unauthorised';
+            //this.toastr.error(error.statusText === 'OK' ?'Unauthorised' : error.statusText, error.status);
+            this.toastr.error(error.error.message, error.error.statusCode);
+          }
+          if (error.status === 404) {
+            error.statusText = 'Not Found';
+           this.toastr.error(error.statusText === 'OK' ? 'Not Found' : error.statusText, error.status);
+            this.router.navigateByUrl('/not-found');
+          }
+          if (error.status === 500) {
+            error.statusText = 'Internal Server Error';
+            //.toastr.error(error.statusText === 'OK' ? 'Internal Server Error' : error.statusText, error.status);
+            const navigationExtras: NavigationExtras = { state: { error: error.error } };
+            this.router.navigateByUrl('/server-error', navigationExtras);
+          }
+
+        }
+        return throwError(error);
+      })
+    );
+    }
+
+}
